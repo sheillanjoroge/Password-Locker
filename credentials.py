c2f30c80009e8e7052c98aab18e20c34201a2dee
sheillanjoroge <sheillan.njoroge@gmail.com>
2020-11-21 17:50:50 +0300

    create class credentials

---
+++ credentials.py
@@ -0,0 +1,9 @@
+class Credentials:
+    '''
+    Class that stores the credetials of the user
+    '''
+
+    def __init__(self, account, email, password):
+        self.account = account
+        self.email = email
+        self.password = password
