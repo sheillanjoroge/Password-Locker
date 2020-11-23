a9fc2616d6cc31cd45719d58f91b60d782785504
sheillanjoroge <sheillan.njoroge@gmail.com>
2020-11-23 04:54:31 +0300

    add a function that gets the credential info of the user

--- credentials.py
+++ credentials.py
@@ -7,3 +7,21 @@ class Credentials:
         self.account = account
         self.email = email
         self.password = password
+    
+    def get_account(self):
+        '''
+        Returns a credentials account and determines which platform the password is used for.
+        '''
+        return self.account
+
+    def get_email(self):
+        '''
+        Get email of the particular credential.
+        '''
+        return self.email  
+
+    def get_password(self):
+        '''
+        Returns a credential's password.
+        '''
+        return self.password
