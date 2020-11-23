ac9e0a0e237f110fb7aed7c5ca8fbecff3f4c22a
sheillanjoroge <sheillan.njoroge@gmail.com>
2020-11-23 05:06:11 +0300

    add IfElse function

--- credentials.py
+++ credentials.py
@@ -25,3 +25,36 @@ class Credentials:
         Returns a credential's password.
         '''
         return self.password
+
+    def set_password(self, password):
+        '''
+        Sets the credential's password.
+        '''
+        self.password = password
+
+    def create_credential(self):
+        '''
+        Stores the credential to the db (csv file)
+        '''
+
+        file_exist = os.path.isfile(Credential.database)
+        with open(Credential.database, 'a')as file_to_write:
+            if not file_exist:
+                fields = ['account', 'email', 'password']
+                file_data = csv.DictWriter(file_to_write, fieldnames=fields, lineterminator='\n')
+                file_data.writeheader()
+                file_data.writerow({
+                    'account': self.get_account(),
+                    'email': self.get_email(),
+                    'password': self.get_password() 
+                })
+                return True
+            else:
+                fields = ['acount', 'email', 'password']
+                file_data = csv.DictWriter(file_to_write, fieldnames=fields, lineterminator='\n')
+                file_data.writerow({
+                    'account': self.get_account(),
+                    'email': self.get_email(),
+                    'password': self.get_password() 
+                })
+                return True
