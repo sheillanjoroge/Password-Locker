5ec88455ce7dcc7c546f97b2af0a5eaf05ae6c7b
sheillanjoroge <sheillan.njoroge@gmail.com>
2020-11-23 06:20:21 +0300

    add docstrings that prompts storage from database

--- user.py
+++ user.py
@@ -19,7 +19,7 @@ class User:
 
     def get_last_name(self):
         '''
-        Return user instsnce lsst name
+        Return user instsnce last name
         '''
         return self.last_name
 
@@ -40,3 +40,94 @@ class User:
         Set user instance password
         '''
         self.password = password
+    
+    def account_created(self):
+        '''
+        Return if a User exists on the db.
+        '''
+        with open(User.database, 'r') as check_creation:
+            csv_data = csv.DictReader(check_creation)
+            for line in csv_data:
+                if line['email'] == self.get_email():   
+                    return True
+            return False
+
+    def create_account(self):
+        '''
+        Create a User instance record on the db.
+        '''
+        fields = ['first_name', 'last_name', 'email', 'password']
+        file_exists = os.path.isfile(User.database)
+        if not file_exists:
+            with open(User.database, 'a') as new_users:
+                csv_writer = csv.DictWriter(new_users, fieldnames=fields, lineterminator='\n')
+                csv_writer.writeheader()
+                csv_writer.writerow({
+                    'first_name': self.get_first_name(),
+                    'last_name': self.get_last_name(),
+                    'email': self.get_email(),
+                    'password': self.get_password()
+                    })
+
+                return True
+        else:
+            if self.account_created() == False:
+                with open(User.database, 'a')as user:
+                    csv_add_user = csv.DictWriter(user, fieldnames=fields, lineterminator='\n')
+                    csv_add_user.writerow({
+                    'first_name': self.get_first_name(),
+                    'last_name': self.get_last_name(),
+                    'email': self.get_email(),
+                    'password': self.get_password()
+                    })
+                    return True
+            else:
+                return False        
+        
+    def delete_account(self):
+        '''
+        Remove a User instance from the db.
+        '''
+        with open(User.database, 'r') as file:
+            csv_file = csv.DictReader(file)
+
+            holder = []
+            counter_before = 0
+            counter_after = 0
+            for line in csv_file:
+                counter_before += 1
+
+                if line['email'] != self.get_email():
+                    holder.append(line)
+
+        with open(User.database, 'w')as to_write:
+            fields = ['first_name', 'last_name', 'email', 'password']
+            csv_writer = csv.DictWriter(to_write, fieldnames=fields, lineterminator='\n')
+            csv_writer.writeheader()
+            
+            for hold in holder:
+                csv_writer.writerow(hold)
+
+
+    def read_file(self):
+        '''
+        Return data from the db.
+        '''
+        try: 
+            with open(User.database, 'r') as opened_file:    
+                return opened_file.read()
+        except FileNotFoundError:
+            return None
+
+
+    @classmethod
+    def check_account_exist(self, email, password):
+        '''
+        Search if a user exists on the db.
+        '''
+        with open(User.database, 'r') as check_creation:
+            csv_data = csv.DictReader(check_creation)
+            for line in csv_data:
+                if line['email'] == email and line['password'] == password :   
+                    return True
+            return False    
